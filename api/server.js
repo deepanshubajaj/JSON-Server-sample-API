96a576d1ebbc16041549a8c1982632d89f42d4e9
deepanshubajaj <deepanshu.bajaj98@gmail.com>
2025-01-07 00:05:12 +0530

    json-server files

--- api/server.js
+++ api/server.js
@@ -7,8 +7,12 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/product/:resource/:id/show': '/:resource/:id'
+    '/api/*': '/$1',           // General route handling for all API requests
+    '/product/:resource/:id/show': '/:resource/:id', // Custom route for products (if needed)
+    '/api/cafes': '/cafes', // Handling cafes requests
+    '/api/cafes/:id': '/cafes/:id', // Handling specific cafe by ID
+    '/api/users': '/users',  // Handling users requests
+    '/api/users/:id': '/users/:id', // Handling specific user by ID
 }))
 server.use(router)
 server.listen(3000, () => {
